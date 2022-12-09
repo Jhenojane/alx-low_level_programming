4e20dde0cef12e27a18247fd05c1e751d3f0f6ad
Jhenojane <jane.agbaohwo@gmail.com>
2022-12-09 10:42:08 -0800

    ‘0x00-hello_world’

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 
 
@@ -16,7 +16,7 @@ int main(void)
 
 {
 
-		puts("Programming is like building a multilingual puzzle\n");
+		puts("\"Programming is like building a multilingual puzzle\n");
 
 			return (0);
 
