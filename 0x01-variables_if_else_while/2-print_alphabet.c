18bcc7bef945702615e4ad40235600bbba063db8
Jhenojane <jane.agbaohwo@gmail.com>
2022-12-09 16:08:48 -0800

    ‘0x01-variables_if_else_while’

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -18,18 +18,12 @@ int main(void)
 
 {
 
-	char low;
-
-
-
-	for (low = 'a'; low <= 'z'; low++)
-
-		putchar(low);
-
-		putchar('\n');
-
-
-
+char low;
+for (low = 'a'; low <= 'z'; low++)
+{
+	putchar(low);
+	putchar('\n');
+}
 	return (0);
 
 }
