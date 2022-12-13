d4244b6b230526acf604af3b9573fd0d5bc38ac8
Jhenojane <jane.agbaohwo@gmail.com>
2022-12-13 15:02:51 -0800

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
+ */
 
 int main(void) {
 	char aputchar[] = { '_','p','u','t','c','h','a','r'} ;
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+** main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
+ */
 
 char _putchar(char c) {
 return (putchar(c));
