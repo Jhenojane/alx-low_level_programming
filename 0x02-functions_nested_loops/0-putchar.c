3b24f84f0ad83406f6bfcecd28613d7479fbbeea
Jhenojane <jane.agbaohwo@gmail.com>
2022-12-13 15:14:26 -0800

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,12 @@
  * Return: 0.
  */
 
-int main(void) {
+int main(void) 
+{
 	char aputchar[] = { '_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-	int length =sizeof(aputchar) / sizeof(aputchar[0]);    
+	int length = sizeof(aputchar) / sizeof(aputchar[0]);    
 	int i;
-		for(i = 0; i < length; i++) 
+		for (i = 0; i < length; i++) 
 			_putchar(aputchar[i]);
 		
 		_putchar('\n');
