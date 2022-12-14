4e8be5357036077e27e06fcd1f5fbc1afc524f92
Jhenojane <jane.agbaohwo@gmail.com>
2022-12-13 20:53:16 -0800

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,17 +8,11 @@
 void print_times_table(int n)
 
 {
-
 	int row;
-
 	int column;
-
 	int product;
-
 	if (n < 0 || n >= 15)
-
 		return;
-
 	for (row = 0; row <= n; row++)
 	{
 		for (column = 0; column <= n; column++)
