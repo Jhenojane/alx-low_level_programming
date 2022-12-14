ff40622a0c4dd18eba6278baeff0ec090b6f2b95
Jhenojane <jane.agbaohwo@gmail.com>
2022-12-13 16:56:17 -0800

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
+ */
+
+void print_alphabet_x10(void);
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+}
+
