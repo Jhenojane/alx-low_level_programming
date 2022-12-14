b332afee8909819f0e9f82f9ccd170c22aa7480a
Jhenojane <jane.agbaohwo@gmail.com>
2022-12-13 19:57:07 -0800

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,15 +3,10 @@
 
 
 /**
-
  * print_last_digit - Check Holberton
-
  * @r: An integer input
-
  * Description: This function prints the last digit of a number
-
  * Return: last digit of number r
-
  */
 
 int print_last_digit(int r)
