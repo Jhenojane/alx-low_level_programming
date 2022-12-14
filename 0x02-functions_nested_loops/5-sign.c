1b45853971cd9fbdb27681de2c7f065e5572e578
Jhenojane <jane.agbaohwo@gmail.com>
2022-12-13 19:43:41 -0800

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,59 +3,53 @@
 
 
 /**
- * print_sign - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_sign - Check Holberton
+ * @n: An input number
+ * Description: Tohis function prints the sign of the number
+ * Return: 1 if number is positive, o if number is 0, or
+ * -1 if number is negative
  */
 
-int main(void)
+int print_sign(int n)
 
 {
 
-  int r;
+	int value;
 
 
 
-  r = print_sign(98);
+	if (n > 0)
 
-  _putchar(',');
+	{
 
-  _putchar(' ');
+		value = 1;
 
-  _putchar(r + '0');
+		_putchar('+');
 
-  _putchar('\n');
+	}
 
-  r = print_sign(0);
+	else if (n == 0)
 
-  _putchar(',');
+	{
 
-  _putchar(' ');
+		value = 0;
 
-  _putchar(r + '0');
+		_putchar('0');
 
-  _putchar('\n');
+	}
 
-  r = print_sign(0xff);
+	else
 
-  _putchar(',');
+	{
 
-  _putchar(' ');
+		value = -1;
 
-  _putchar(r + '0');
+		_putchar('-');
 
-  _putchar('\n');
+	}
 
-  r = print_sign(-1);
 
-  _putchar(',');
 
-  _putchar(' ');
-
-  _putchar(r + '0');
-
-  _putchar('\n');
-
-  return (0);
+	return (value);
 
 }
