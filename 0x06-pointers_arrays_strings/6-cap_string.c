5445a9e5f66990338232167a11725dd93f8da9a8
Jhenojane <jane.agbaohwo@gmail.com>
2022-12-23 15:25:24 -0800

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,52 +3,40 @@
 
 
 /**
- * cap_string - capitalizes all words in a string
- * @s: string
- * Return: address of s
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ * Return: the resulting string
  */
 
 char *cap_string(char *s)
 
 {
 
-	int i = 0, j;
-
-	char a[] = " \t\n,;.!?\"(){}";
+	int i, j;
 
 
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
 
-	while (*(s + i))
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
 
 	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			s[i] -= 32;
 
+		for (j = 0; j < 13; j++)
 		{
-
-			if (i == 0)
-
-				*(s + i) -= 'a' - 'A';
-
-			else
-
+			if (s[i] == spe[j])
 			{
-
-				for (j = 0; j <= 12; j++)
-
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
 
-					if (a[j] == *(s + i - 1))
-
-						*(s + i) -= 'a' - 'A';
-
+					s[i + 1] -= 32;
 				}
-
 			}
-
 		}
-
-		i++;
 
 	}
 
