b6346bc25a85319797014b9b4fb93664670b9024
Jhenojane <jane.agbaohwo@gmail.com>
2023-01-02 08:57:18 -0800

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * _strstr - locates a substring
  * @haystack: the longer string to search
@@ -12,27 +10,20 @@
 char *_strstr(char *haystack, char *needle)
 
 {
-
 	unsigned int i = 0, j = 0;
-
-
 
 	while (haystack[i])
 
 	{
-
 		while (needle[j] && (haystack[i] == needle[0]))
 
 		{
-
 			if (haystack[i + j] == needle[j])
-
 				j++;
 
 			else
 
 				break;
-
 		}
 
 		if (needle[j])
@@ -40,15 +31,11 @@ char *_strstr(char *haystack, char *needle)
 		{
 
 			i++;
-
 			j = 0;
-
 		}
 
 		else
-
 			return (haystack + i);
-
 	}
 
 	return (0);
