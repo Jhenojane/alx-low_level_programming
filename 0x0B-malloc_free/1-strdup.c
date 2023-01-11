cde1c9445d52c691c7fd6be74708fb0f21e51065
Jhenojane <jane.agbaohwo@gmail.com>
2023-01-11 01:54:01 -0800

    0x0B-malloc_free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -32,28 +32,19 @@ char *_strdup(char *str)
 		return (NULL);
 
 
-
 	while (str[len])
 
 		len++;
 
-
-
 	dup = malloc(sizeof(char) * (len + 1));
-
-
 
 	if (dup == NULL)
 
 		return (NULL);
 
-
-
 	while ((dup[i] = str[i]) != '\0')
 
 		i++;
-
-
 
 	return (dup);
 
