5e12298f6fa6a21c25b68ef02a855ce6992c43bc
Jhenojane <jane.agbaohwo@gmail.com>
2023-01-12 05:20:42 -0800

    0x0B-malloc_free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -16,7 +16,6 @@ char *argstostr(int ac, char **av)
 
 	int arg, byte, index, size = ac;
 
-
 	if (ac == 0 || av == NULL)
 
 		return (NULL);
@@ -26,41 +25,27 @@ char *argstostr(int ac, char **av)
 	{
 
 		for (byte = 0; av[arg][byte]; byte++)
-
 			size++;
 
 	}
 
-
-
 	str = malloc(sizeof(char) * size + 1);
-
-
 
 	if (str == NULL)
 
 		return (NULL);
 
-
-
 	index = 0;
-
-
 
 	for (arg = 0; arg < ac; arg++)
 
 	{
-
 		for (byte = 0; av[arg][byte]; byte++)
-
 			str[index++] = av[arg][byte];
-
 		str[index++] = '\n';
-
 	}
 
 	str[size] = '\0';
-
 	return (str);
 
 }
