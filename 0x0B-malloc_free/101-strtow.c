6b597bb108d6c942c5ac66c786be90b780e7dee4
Jhenojane <jane.agbaohwo@gmail.com>
2023-01-12 05:32:53 -0800

    0x0B-malloc_freeclear

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,24 +12,20 @@ int count_word(char *s)
 {
 	int flag, c, w;
 	flag = 0;
+
 	w = 0;
 
 	for (c = 0; s[c] != '\0'; c++)
-
 	{
-
 		if (s[c] == ' ')
 			flag = 0;
 		else if (flag == 0)
-
 		{
 			flag = 1;
 			w++;
 		}
 	}
-
 	return (w);
-
 }
 
 /**
@@ -46,19 +42,13 @@ char **strtow(char *str)
 	while (*(str + len))
 		len++;
 	words = count_word(str);
-
 	if (words == 0)
-
 		return (NULL);
 
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
-
 	if (matrix == NULL)
-
 		return (NULL);
-
 	for (i = 0; i <= len; i++)
-
 	{
 		if (str[i] == ' ' || str[i] == '\0')
 
@@ -76,15 +66,11 @@ char **strtow(char *str)
 				matrix[k] = tmp - c;
 				k++;
 				c = 0;
-
 			}
 		}
 		else if (c++ == 0)
 			start = i;
 	}
-
 	matrix[k] = NULL;
 	return (matrix);
 }
-
-
